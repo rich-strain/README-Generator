0d4d57c274f26b3a8600f085acc3528f4877d9f4
Rich <richardstrain@gmail.com>
2024-07-11 08:54:10 -0400

    Console Log Inquirer Responses

--- index.js
+++ index.js
@@ -57,7 +57,11 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    console.log(data);
+  });
+}
 
 // Function call to initialize app
 init();
