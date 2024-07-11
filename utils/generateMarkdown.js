33d014d9c1aabc313287e0d9342c5e761f0d4507
Rich <richardstrain@gmail.com>
2024-07-11 10:45:00 -0400

    Render License Link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,7 +9,7 @@ function renderLicenseBadge(license) {
     case 'Apache':
       return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
     case 'Creative Commons License':
-      return '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)';
+      return '[![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)';
     case 'None':
       return 'LICENSE: NONE';
   }
@@ -18,7 +18,19 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  // switch statement will go here to return the correct link based on the license paramater
+  // switch statement to return the correct link based on the license paramater
+  switch (license) {
+    case 'MIT':
+      return '[MIT License](https://opensource.org/licenses/MIT)';
+    case 'GNU General Public License':
+      return '[GNU General Public License](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'Apache':
+      return '[Apache License](https://opensource.org/licenses/Apache-2.0)';
+    case 'Creative Commons License':
+      return '[Creative Commons License](http://creativecommons.org/publicdomain/zero/1.0/)';
+    case 'None':
+      return 'LICENSE: NONE';
+  }
 }
 
 // TODO: Create a function that returns the license section of README
@@ -33,7 +45,7 @@ function generateMarkdown(data) {
   // Currently Only Retuns The Title Of The Project and Section Headers
   return `# ${data.title} \n ${renderLicenseBadge(data.license)} \n 
   ## Description \n ## Table Of Contents \n ## Usage \n 
-  ## License \n ## Contributing \n ## Tests \n ## Questions \n `;
+  ## License \n ${renderLicenseLink(data.license)} \n ## Contributing \n ## Tests \n ## Questions \n `;
 }
 
 module.exports = generateMarkdown;
